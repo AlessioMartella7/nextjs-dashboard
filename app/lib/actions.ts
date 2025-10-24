04707f92ef69058c0ba5dbecbdc418bd8364a481
Alessio_Martella <a.martella@sctechnology.it>
2025-10-24 12:14:45 +0200

    fix:unexpected types for actions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -57,9 +57,6 @@ export async function updateInvoice(id: string, formData: FormData) {
   `;
   } catch (error) {
     console.error(error);
-    return {
-      message: "Database Error: Failed to Update Invoice. ",
-    };
   }
 
   revalidatePath("/dashboard/invoices");
